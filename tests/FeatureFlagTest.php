5cc7a524fd7d5b58c4d829b702766fbda58ac4c2
Matteo Greco <matteo@matteogreco.net>
2024-12-09 13:42:55 +0100

    testing plan

--- tests/FeatureFlagTest.php
+++ tests/FeatureFlagTest.php
@@ -8,4 +8,11 @@ use DigitalGravy\FeatureFlag\FeatureFlag;
 class FeatureFlagTest extends TestCase
 {
 
+    /**
+     * - Digital Gravy plugin authors can share feature flags across website
+     * - The website stores the feature flags in a keyed array
+     * - Feature flag keys are unique alphanumeric strings
+     * - Feature flags can be 'on' or 'off'
+     * - DG plugin authors can override feature flags with a local .env file
+     */
 }
